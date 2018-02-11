4e70853bd1f60ec3a34f814ffdca6b7bb9c510de
Sriram <sriramgeekinside@gmail.com>
2018-02-11 22:30:09 +0530

    files modified

--- routes/posts.js
+++ routes/posts.js
@@ -1,34 +1,8 @@
 var express = require('express')
 var app = express()
 var router = express.Router();
-var catalogues = require('../src/controller/postcontroller');
-
-var app_name = "Red leaf | "
-// SHOW LIST OF POSTS
-app.get('/', function(req, res, next) {
-	req.getConnection(function(error, conn) {
-		conn.query('SELECT * FROM posts ORDER BY id DESC',function(err, rows, fields) {
-            //if(err) throw err
-			if (err) {
-				req.flash('error', err)
-				res.render('user/list', {
-					title: 'User List', 
-					data: ''
-				})
-			} else {
-				// render to views/user/list.ejs template file
-				res.render('post/list', {
-					title: app_name + 'Virtual movie', 
-					data: rows
-				})
-			}
-		})
-	})
-})
-
-router.route('/catalogues')
-    .get(catalogues.apiGET)
-    .post(catalogues.apiPOST);
+var posts = require('../src/controller/postcontroller');
+var app_name = "Red leaf | ";
+router.route('/').get(posts.allPosts);
+router.route('/post/(:id)').get(posts.show);
 module.exports = router;
-
-//module.exports = app
--- src/controller/postcontroller.js
+++ src/controller/postcontroller.js
@@ -1,6 +1,6 @@
 var request = require('request');
 var app_name = "Red leaf | "
-exports.apiGET = function(req, res) {
+exports.allPosts = function(req, res) {
   // var options = prepareCataloguesAPIHeaders(req);
   // request(options, function(err, response, body){
   //   res.send(body);
@@ -19,6 +19,27 @@ exports.apiGET = function(req, res) {
 				res.render('post/list', {
 					title: app_name + 'Virtual movie', 
 					data: rows
+				})
+			}
+		})
+	})
+};
+exports.show = function(req, res) {
+	var id = req.params.id; 
+	req.getConnection(function(error, conn) {
+		conn.query('SELECT * FROM posts where id ='+id,function(err, rows, fields) {
+            //if(err) throw err
+			if (err) {
+				req.flash('error', err)
+				res.render('user/list', {
+					title: 'User List', 
+					data: ''
+				})
+			} else {
+				// render to views/user/list.ejs template file
+				res.render('post/show', {
+					title: app_name + rows[0].title, 
+					data: rows[0]
 				})
 			}
 		})
