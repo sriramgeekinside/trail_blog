7488fa6eb577cd3587ca3990e95aba79b19fdff0
Sriram <sriramgeekinside@gmail.com>
2018-03-16 23:06:56 +0530

    Node js login work regarding

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,4 +1,8 @@
 var request = require('request');
+var bcrypt = require('bcrypt');
+const saltRounds = 10;
+const myPlaintextPassword = 's0/\/\P4$$w0rD';
+const someOtherPlaintextPassword = 'not_bacon';
 var app_name = "Red leaf | "
 exports.login = function (req, res) { 
     // render to views/login.ejs template file
@@ -14,4 +18,19 @@ exports.authenticate = function (req, res) {
     } else {
         res.redirect('/admin/login');
     }
+};
+exports.store = function (req, res) {
+    var user = req.body;
+    console.log(user);
+    bcrypt.genSalt(saltRounds, function(err, salt) {
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            var sql = "INSERT INTO users (name, email,password) VALUES ('"+user.username+"','"+user.email+"', '"+hash+"')";
+            req.getConnection(function(error, conn) {    
+            conn.query(sql, function (err, result) {
+                if (err) throw err;
+                    resolve(result.length > 0);    
+                });
+            });
+        });
+    });
 };
